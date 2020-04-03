8a51736631b65cc907ac0dd1aa829eb501b74139
Brian DeCost <brian.decost@nist.gov>
2020-04-03 10:25:47 -0400

    add script to dump v2 data from json strings to separate csv files (with integer ids that correspond to experiment ids)

---
+++ nitial/export_data.py
@@ -0,0 +1,45 @@
+""" older version of db stores e-chem data directly as a json string.
+
+this script extracts that json data to csv files, and removes autoranging artifacts in the process.
+"""
+import os
+import json
+import cycvolt
+import dataset
+import numpy as np
+import pandas as pd
+
+def load_cv(jsondata):
+    cvdata = json.loads(jsondata)
+    del cvdata['error_codes']
+    del cvdata['current_range']
+    return pd.DataFrame(cvdata)
+
+def remove_autorange_artifacts(data):
+    I = data['current']
+    V = data['potential']
+    segment = data['segment']
+
+    log_I = np.zeros_like(I)
+    for s in segment.unique():
+        mask = segment == s
+        absval = np.abs(I[mask])
+        _log_current = np.log(np.clip(absval, absval[absval > 0].min(), np.inf))
+        a = cycvolt.analyze.model_autorange_artifacts(V[mask], I[mask])
+        _log_current = _log_current - a
+        log_I[mask] = _log_current
+
+    return np.exp(log_I)
+
+if __name__ == '__main__':
+
+    db = dataset.connect('sqlite:///data/k20-NiTiAl-v2.db')
+    expt = db['experiment']
+
+    os.makedirs('data/k20-v2-cvs', exist_ok=True)
+
+    for row in expt.all():
+
+        cvdata = load_cv(row['results'])
+        cvdata['corrected_current'] = remove_autorange_artifacts(cvdata)
+        cvdata.to_csv(f"data/k20-v2-cvs/cv_{row['id']:04d}.csv")
